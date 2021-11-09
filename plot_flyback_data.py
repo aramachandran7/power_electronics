6727173607a725ab047b2ef9878624782aa41638
Adi <aramachandran@olin.edu>
2021-11-08 22:40:30 -0500

    lb7 stuff

--- plot_flyback_data.py
+++ plot_flyback_data.py
@@ -3,30 +3,61 @@ from numpy import arange
 import pandas as pd
 # parse logfile
 
-df_4 = pd.read_csv( './flyback_data/4v.csv')
-df_4o = pd.read_csv('./flyback_data/4vo.csv')
-df_7 = pd.read_csv( './flyback_data/7vo.csv')
-df_7o = pd.read_csv('./flyback_data/7vo2.csv')
-df_9 = pd.read_csv( './flyback_data/9v.csv')
-df_9o = pd.read_csv('./flyback_data/9vo.csv')
+df_base= pd.read_csv( './lb7_data/base.csv')
+df_base_zoom = pd.read_csv( './lb7_data/base2.csv')
+df_clp_8 = pd.read_csv( './lb7_data/clp8_.csv')
+df_clp_8_zoom = pd.read_csv( './lb7_data/clp8.csv')
+df_clp_10 = pd.read_csv( './lb7_data/clp10.csv')
+df_clp_10_zoom = pd.read_csv( './lb7_data/clp10_.csv')
+df_sbh= pd.read_csv( './lb7_data/sbh.csv')
+df_sbh_zoom = pd.read_csv( './lb7_data/sbh2.csv')
+df_sbl = pd.read_csv('./lb7_data/sbl62.csv')
+df_sbl_zoom = pd.read_csv( './lb7_data/sbl62_.csv')
+
+# df_list = [
+#     df_4, 
+#     df_4o,
+#     df_7,
+#     df_7o,
+#     df_9,
+#     df_9o,
+# ]
 
 df_list = [
-    df_4, 
-    df_4o,
-    df_7,
-    df_7o,
-    df_9,
-    df_9o,
+    df_base     , 
+df_base_zoom, 
+df_clp_8, 
+df_clp_8_zoom, 
+df_clp_10, 
+df_clp_10_zoom, 
+df_sbh, 
+df_sbh_zoom, 
+df_sbl, 
+df_sbl_zoom, 
 ]
 
+# title_list = [
+#     '4V', 
+#     '4V_zoom',
+#     '7V',
+#     '7V_zoom',
+#     '9V' ,
+#     '9V_zoom',
+# ]
+
 title_list = [
-    '4V', 
-    '4V_zoom',
-    '7V',
-    '7V_zoom',
-    '9V' ,
-    '9V_zoom',
+'baseline', 
+'baseline zoomed', 
+'clamp 8V', 
+'clamp 8V zoomed', 
+'clamp 10V', 
+'clamp 10V zoomed', 
+'snubber high frequency', 
+'snubber high frequency zoomed', 
+'snubber low frequency ', 
+'snubber low frequency zoomed', 
 ]
+
 for i,df in enumerate(df_list):
 
     # grab timesteps, start time
@@ -39,48 +70,64 @@ for i,df in enumerate(df_list):
     df = df.drop(df.columns[-1], axis = 1) # last col, unnamed
 
     df['X'] = time_increment * df['X'].astype(float) # x is now time
-    df['CH1'] = df['CH1'].astype(float)
-    df['CH2'] = df['CH2'].astype(float)
+    # df['CH1'] = df['CH1'].astype(float)
+    # df['CH2'] = df['CH2'].astype(float)
     df['CH3'] = df['CH3'].astype(float)
     df['CH4'] = df['CH4'].astype(float)
 
-
+    
     # plot CH1
-    df.plot(x='X',y='CH1')
-    plt.legend(loc='best')
-    plt.title('Voltage across shunt resistor as a function of time, output voltage = ' + title_list[i])
-    plt.xlabel('Time, (seconds)')
-    plt.ylabel('Voltage across shunt resistor, (volts)')
-    plt.savefig(f'./saved_flyback_figs/Vsh_{title_list[i]}.png')
+    # df.plot(x='X',y='CH1')
+    # plt.legend(loc='best')
+    # plt.title('Voltage across shunt resistor as a function of time, output voltage = ' + title_list[i])
+    # plt.xlabel('Time, (seconds)')
+    # plt.ylabel('Voltage across shunt resistor, (volts)')
+    # # plt.axis([x_min, x_max, y_min, y_max])
+    # plt.savefig(f'./figs_lb6/Vsh_{title_list[i]}.png')
 
-    # plot CH2
-    df.plot(x='X',y='CH2')
-    plt.legend(loc='best')
-    plt.title('NMOS Drain Voltage as a function of time, output  voltage = ' + title_list[i])
-    plt.xlabel('Time, (seconds)')
-    plt.ylabel('NMOS Drain Voltage (volts)')
-    plt.savefig('foo.png')
-    plt.savefig(f'./saved_flyback_figs/Vd_{title_list[i]}.png')
+
+    # df.iloc[int(len(df.index)/2):int(len(df.index)*3/4)].plot(x='X',y='CH1')
+    # plt.legend(loc='best')
+    # plt.title('Voltage across shunt resistor as a function of time, output voltage = ' + title_list[i])
+    # plt.xlabel('Time, (seconds)')
+    # plt.ylabel('Voltage across shunt resistor, (volts)')
+    # plt.savefig(f'./figs_lb6/Vsh_{title_list[i]}.png')
+
+    if set(['CH1']).issubset(df.columns):
+        df['CH1'] = df['CH1'].astype(float)
+
+        # df['CH2'] = df['CH2'].astype(float)
+
+        # plot ch2 for diode cathode
+        # plot CH2
+        df.plot(x='X',y='CH1')
+        plt.legend(loc='best')
+        plt.title('Clamp Diode Cathode Voltage as a function of time, ' + title_list[i])
+        plt.xlabel('Time, (seconds)')
+        plt.ylabel('NMOS Drain Voltage (volts)')
+        plt.savefig(f'./figs_lb7/Vcathode_{title_list[i]}.png')
 
     # try: # channel 3
     df.plot(x='X',y='CH3')
     plt.legend(loc='best')
-    plt.title('Output Voltage as a function of time, output voltage RMS = ' + title_list[i])
+    plt.title('Voltage across shunt resistor as a function of time, ' + title_list[i])
     plt.xlabel('Time, (seconds)')
-    plt.ylabel('Output Voltage (volts)')
-    plt.savefig(f'./saved_flyback_figs/Vin_{title_list[i]}.png')
+    plt.ylabel('Shunt Voltage (volts)')
+    plt.savefig(f'./figs_lb7/Vsh_{title_list[i]}.png')
     # except: 
     #     print(title_list[i] + ' does not have output voltage logged')
 
     # try: # channel 4
     df.plot(x='X',y='CH4')
     plt.legend(loc='best')
-    plt.title('Input Voltage as a function of time, output voltage RMS = ' + title_list[i])
+    plt.title('NMOS Drain Voltage as a function of time, ' + title_list[i])
     plt.xlabel('Time, (seconds)')
-    plt.ylabel('Input Voltage (volts)')
-    plt.savefig(f'./saved_flyback_figs/Vin_{title_list[i]}.png')
+    plt.ylabel('NMOS Drain Voltage (volts)')
+    plt.savefig(f'./figs_lb7/Vd_{title_list[i]}.png')
     # except: 
     #     print(title_list[i] + ' does not have input voltage logged')
+    # if i>2: 
+    #     break
     
 
 plt.show()
